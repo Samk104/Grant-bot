6896adf2d19187dbb434df2e19f2630048d10853
Samujjwal Kumar <kumarsamujjwal@gmail.com>
2025-02-17 20:26:12 -0500

    Fixed typescript error

--- app/api/get-pdf/route.ts
+++ app/api/get-pdf/route.ts
@@ -1,15 +1,3 @@
-/**
- * @swagger
- * /api/getPDF:
- *   get:
- *     description: Returns all the PDF documents found on a web page
- *     responses:
- *       200:
- *         description: List of links to PDF documents
- *       400:
- *         description: Link not found
- */
-
 import axios from "axios";
 import * as cheerio from "cheerio";
 import { NextResponse } from "next/server";
@@ -37,16 +25,23 @@ export async function POST(req: Request) {
           const absoluteUrl = new URL(href, link).href;
           pdfLinks.push(absoluteUrl);
         } catch {
-          return NextResponse.json(
-            { error: "Invalid URL found in the webpage." },
-            { status: 400 }
-          );
+          
+          throw new Error("Invalid URL found in the webpage.");
         }
       }
+      
     });
 
     return NextResponse.json({ pdfLinks }, { status: 200 });
   } catch (error: unknown) {
+    
+    if (error instanceof Error && error.message === "Invalid URL found in the webpage.") {
+      return NextResponse.json(
+        { error: "Invalid URL found in the webpage." },
+        { status: 400 }
+      );
+    }
+    
     if (axios.isAxiosError(error) && error.response?.status === 404) {
       return NextResponse.json(
         { error: "The page was not found." },
